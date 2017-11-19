84624785da5b97aad1f1621ba04564d0ed8d27cf
askariya <arunaluk@yahoo.ca>
2017-11-19 01:30:40 -0700

    Adding error checking for packets

--- fw.py
+++ fw.py
@@ -5,7 +5,7 @@ import sys
 rules = [] # global list to hold a set of rules (dictionaries)
 
 # returns True if line is valid rule, False otherwise
-def validate_line(line):
+def check_rule(line):
     return True #TODO perform all error checking for the line here
 
 # returns a ditionary containing the rule from the argument
@@ -35,15 +35,67 @@ def read_config(filename):
             line = line.strip()
             line = line.split()
             # check if the line contains a valid rule
-            if validate_line(line):
+            if check_rule(line):
                 # create and add the rule to the rules list
                 rules.append(create_rule(line))
 
+# creates and returns a dictionary containing a packet
+def create_packet(line):
+    packet = {}
+    line = line.split()
+    if len(line) != 4:
+        return None, False
+
+    packet["direction"] = line[0]
+    packet["ip"] = line[1]
+    packet["port"] = line[2]
+    packet["flag"] = line[3]
+
+    # ERROR CHECKING
+    # direction
+    if packet["direction"] not in ("in", "out"):
+        return None, False
+
+    # IP
+    ip = packet["ip"].split(".")
+    if len(ip) != 4:
+        return None, False
+    try:
+        for num in ip:
+            if int(num) not in range (0, 256):
+                return None, False
+    except:
+        return None, False
+
+    # port
+    try:
+        if int(packet["port"]) not in range(0, 65536):
+            return None, False
+    except:
+        return None, False
+
+    # flag
+    if packet["flag"] not in ("0", "1"):
+        return None, False
+
+    # successful packet -- no errors
+    return packet, True
+
+# checks the packet against the rules
+def validate_packet(packet):
+    pass # TODO figure out how to start this
+
+# reads in the packets from std input
 def read_input_packets():
-    print(rules)
     for line in sys.stdin:
         line = line.strip() # strip extra whitespace
-        print(line)
+        packet, valid = create_packet(line)
+        # if the packet is invalid, skip
+        if not valid:
+            print("Packet Error")
+            continue
+        validate_packet(packet)
+        print(packet)
 
 # argparse function to handle user input
 # Reference: https://docs.python.org/3.6/howto/argparse.html
