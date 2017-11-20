cfe1e9e242ea7ba73c7fc40e98939f7c4ca6a7ff
askariya <arunaluk@yahoo.ca>
2017-11-19 20:54:11 -0700

    Adding completed code (maybe some error checks left)

--- fw.py
+++ fw.py
@@ -1,31 +1,9 @@
 import argparse
 import string
 import sys
+import funcs
 
 rules = [] # global list to hold a set of rules (dictionaries)
-
-# returns True if line is valid rule, False otherwise
-def check_rule(line):
-    return True #TODO perform all error checking for the line here
-
-# returns a ditionary containing the rule from the argument
-def create_rule(line):
-    rule = {}
-    if (len(line) == 4):
-        rule["direction"] = line[0]
-        rule["action"] = line[1]
-        rule["ip"] = line[2]
-        rule["ports"] = line[3]
-        rule["flag"] = None
-    elif (len(line) == 5):
-        rule["direction"] = line[0]
-        rule["action"] = line[1]
-        rule["ip"] = line[2]
-        rule["ports"] = line[3]
-        rule["flag"] = line[4]
-    else:
-        raise ValueError("Error: Invalid line")
-    return rule
 
 # read in the firewall rules from the config file
 def read_config(filename):
@@ -35,76 +13,21 @@ def read_config(filename):
             line = line.strip()
             line = line.split()
             # check if the line contains a valid rule
-            if check_rule(line):
+            rule, correct = funcs.create_rule(line)
+            if correct:
                 # create and add the rule to the rules list
-                rules.append(create_rule(line))
-
-# creates and returns a dictionary containing a packet
-def create_packet(line):
-    packet = {}
-    line = line.split()
-    if len(line) != 4:
-        return None, False
-
-    packet["direction"] = line[0]
-    packet["ip"] = line[1]
-    packet["port"] = line[2]
-    packet["flag"] = line[3]
-
-    # ERROR CHECKING
-    # direction
-    if packet["direction"] not in ("in", "out"):
-        return None, False
-
-    # IP
-    ip = packet["ip"].split(".")
-    if len(ip) != 4:
-        return None, False
-    try:
-        for num in ip:
-            if int(num) not in range (0, 256):
-                return None, False
-    except:
-        return None, False
-
-    # port
-    try:
-        if int(packet["port"]) not in range(0, 65536):
-            return None, False
-    except:
-        return None, False
-
-    # flag
-    if packet["flag"] not in ("0", "1"):
-        return None, False
-
-    # successful packet -- no errors
-    return packet, True
-
-# checks the packet against the rules
-def validate_packet(packet):
-    for rule in rules:
-        # check direction
-        if packet["direction"] != rule["direction"]:
-            continue
-        # check IP
-        # check port
-        # check flag
-
-def check_IP():
-    pass # TODO Compare the IPs here
+                rules.append(rule)
 
 # reads in the packets from std input
 def read_input_packets():
     for line in sys.stdin:
         line = line.strip() # strip extra whitespace
-        packet, valid = create_packet(line)
+        packet, correct = funcs.create_packet(line)
         # if the packet is invalid, skip
-        if not valid:
-            # print("Packet Error")
+        if not correct:
             continue
-        validate_packet(packet)
-        # print(packet)
+        # print output
+        print(funcs.validate_packet(rules, packet))
 
 # argparse function to handle user input
 # Reference: https://docs.python.org/3.6/howto/argparse.html
@@ -117,10 +40,6 @@ def parse_arguments():
                         help="file containing firewall rules",
                         type=str)
     args = parser.parse_args()
-
-    # if args.port < 0 or args.port > 65535:
-    #     parser.exit("usage: " + usage_string)
-
     return args
 
 
